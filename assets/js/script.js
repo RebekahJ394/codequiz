3b54a6ef3acf806b6c1f309b5e9d70e446ba14ec
Rebekah Jones <rebekahmez@gmail.com>
2021-07-22 23:05:53 -0400

    HTML and CSS additions/1 question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,16 @@
 var startbutton = document.querySelector(".start-button");
+var correctAnswer=()
+// fuction startGame() {
 
-fuction startGame() {
-
-}
+// }
 
 function startTimer() {
+    timeLeft= 10
+
     timer= setInterval(function) {
-        
+        timerCount--
+  if (timeLeft > 0)   {
+
+  }   
     }
 }

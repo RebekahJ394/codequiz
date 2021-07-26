24647c2b0c040ebba02d1aa42015d8e33fbbb763
Rebekah Jones <rebekahmez@gmail.com>
2021-07-25 23:48:35 -0400

    Successfully populates first question after clicking start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 var questions = [
     {
-        numb:1,
         question: "What does HTML stand for?",
         answer: "Hyper Text Markup Language",
         options: [
@@ -11,7 +10,6 @@ var questions = [
         ]
     },
     {
-        numb: 2,
         question: "What does CSS stand for?",
         answer: "Cascading Style Sheet",
         options: [
@@ -22,7 +20,6 @@ var questions = [
         ]
      },
     {
-        numb: 3,
         question: "What does a String represent?",
         answer: "Text",
         options: [
@@ -33,7 +30,6 @@ var questions = [
         ]
     },
     {
-        numb: 4,
         question: "What's the most common title for your HTML code to be stored?",
         answer: "index.html",
         options: [
@@ -44,7 +40,6 @@ var questions = [
         ]
     },
         {
-        numb: 5,
         question: "Who is considered the 'father of the computer'?",
         answer: "Charles Babbage",
         options: [
@@ -62,31 +57,40 @@ var quizbox= document.querySelector(".quizbox")
 var finishedbox =document.querySelector('#finishedbox')
 var resultbox =document.querySelector('#resultsbox')
 var restartbutton= document.querySelector("#restart-button");
-var questionsection=document.querySelector("#questiontext");
-var firstoption= document.querySelector("#answer1");
-var secondoption= document.querySelector("#answer2");
-var thirdoption= document.querySelector("#answer3");
-var fourthoption= document.querySelector("#answer4");
+
+var questionsection=document.getElementById("questiontext");
+var firstoption= document.getElementById("answer1");
+var secondoption= document.getElementById("answer2");
+var thirdoption= document.getElementById("answer3");
+var fourthoption= document.getElementById("answer4");
 // var correctAnswer= ();
 // fuction startGame() {
 
 // }
 console.log(questions[0])
 startbutton.addEventListener("click", function() {
-    playquiz(questions);
+    playquiz();
 })
 
 function playquiz(){
     // quiz=setUpQuestions(setQuestion);
     quizbox.style.removeProperty("display");
     starterbox.setAttribute("style","display:none");
-    setUpQuestions(0)
+    setUpQuestions(1)
 }
 
 function setUpQuestions(){
-    questionsection.textContent= questions.question;
+    questionsection.textContent = questions[0].question;
+    firstoption.textContent = questions[0].options[0];
+    secondoption.textContent = questions[0].options[1];
+    thirdoption.textContent = questions[0].options[2];
+    fourthoption.textContent = questions[0].options[3];
     
 }
+
+firstoption.addEventListener("click", function() {
+    console.log("this was clicked")
+})
 // function setUpQuestions() {
 
 // }
